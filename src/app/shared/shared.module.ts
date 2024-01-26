19c1b6118a8755c349010c3ca6e0fa8335ac1612
zeotoni <zeotoni666@gmail.com>
2024-01-26 15:40:54 -0300

    feat: add header component

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,10 +2,11 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { ButtonComponent } from './button/button.component';
 import { ContainerComponent } from './container/container.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
-  declarations: [ButtonComponent, ContainerComponent],
+  declarations: [ButtonComponent, ContainerComponent, HeaderComponent],
   imports: [CommonModule],
-  exports: [ButtonComponent, ContainerComponent],
+  exports: [ButtonComponent, ContainerComponent, HeaderComponent],
 })
 export class SharedModule {}
